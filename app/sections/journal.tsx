511f578fc1b321ed8373c8aef15cd4ada613cc27
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-01-29 07:56:44 +0000

    added journal lambo links

--- app/sections/journal.tsx
+++ app/sections/journal.tsx
@@ -1,7 +1,36 @@
+import Link from "next/link"
+
 export const Journal = () => {
   return (
     <section id="journal" className="flex flex-col gap-2">
       <h2 className="text-slate-500">Journal</h2>
+      <ul>
+        <li className="-mx-3">
+          <Link href={""} className="flex items-center justify-between gap-2 p-3 sm:gap-3 active:scale-[0.98] text-accent hover:bg-white/5 transition-all rounded-2xl hover:ring-1 ring-border outline-none focus-visible:ring-2 focus-visible:ring-accent">
+            Activo
+          </Link>
+        </li>
+        <li className="-mx-3">
+          <Link href={""} className="flex items-center justify-between gap-2 p-3 sm:gap-3 active:scale-[0.98] text-accent hover:bg-white/5 transition-all rounded-2xl hover:ring-1 ring-border outline-none focus-visible:ring-2 focus-visible:ring-accent">
+            Activo
+          </Link>
+        </li>
+        <li className="-mx-3">
+          <Link href={""} className="flex items-center justify-between gap-2 p-3 sm:gap-3 active:scale-[0.98] text-accent hover:bg-white/5 transition-all rounded-2xl hover:ring-1 ring-border outline-none focus-visible:ring-2 focus-visible:ring-accent">
+            Activo
+          </Link>
+        </li>
+        <li className="-mx-3">
+          <Link href={""} className="flex items-center justify-between gap-2 p-3 sm:gap-3 active:scale-[0.98] text-accent hover:bg-white/5 transition-all rounded-2xl hover:ring-1 ring-border outline-none focus-visible:ring-2 focus-visible:ring-accent">
+            Activo
+          </Link>
+        </li>
+        <li className="-mx-3">
+          <Link href={""} className="flex items-center justify-between gap-2 p-3 sm:gap-3 active:scale-[0.98] text-accent hover:bg-white/5 transition-all rounded-2xl hover:ring-1 ring-border outline-none focus-visible:ring-2 focus-visible:ring-accent">
+            Activo
+          </Link>
+        </li>
+      </ul>
     </section>
   )
 }
