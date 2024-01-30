7b377bd229aa8e24c65a12b7d0742490b501a74b
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-01-30 09:03:59 +0000

    mod: nav items to lowercase, reduced x padding

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,26 +3,26 @@ import Link from "next/link"
 const Navbar = () => {
   const nav = [
     {
-      label: "About",
+      label: "about",
       route: "/about"
     },
     {
-      label: "Blog",
+      label: "blog",
       route: "/blog"
     },
     {
-      label: "Store",
+      label: "store",
       route: "/store"
     },
   ]
   return (
     <header className="w-full px-4 border-b">
-      <nav className="py-6 max-w-screen-md mx-auto flex justify-between">
+      <nav className="flex justify-between max-w-screen-md py-6 mx-auto">
         <h2>Enric Trillo</h2>
         <ul className="flex gap-2">
           {nav.map((i, idx) => (
             <li key={idx}>
-              <Link className="hover:bg-gray-300 px-4 rounded-md py-2" href={i.route}>{i.label}</Link>
+              <Link className="px-2 py-2 rounded-md hover:bg-gray-300" href={i.route}>{i.label}</Link>
             </li>
           ))}
         </ul>
