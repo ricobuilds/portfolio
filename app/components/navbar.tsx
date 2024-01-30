223996792acfb6b3481d3f722046a18a4eefce37
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-01-30 09:20:22 +0000

    added negative margin to nav items

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <header className="w-full px-4 border-b">
       <nav className="flex justify-between max-w-[696px] py-6 mx-auto">
         <h2>Enric Trillo</h2>
-        <ul className="flex gap-2">
+        <ul className="flex gap-2 -mr-2">
           {nav.map((i, idx) => (
             <li key={idx}>
               <Link className="px-2 py-2 rounded-md hover:bg-gray-300" href={i.route}>{i.label}</Link>
