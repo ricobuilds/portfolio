6546b7b77357dee6a0fcc8b926085b91b773e0e5
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-01-30 09:10:51 +0000

    updated home width

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Journal } from "./sections/journal";
 export default function Home() {
   return (
     <main className="w-full px-4">
-    <div className="flex flex-col max-w-screen-md min-h-screen gap-16 py-24 mx-auto">
+      <div className="flex flex-col max-w-[696px] min-h-screen gap-16 py-24 mx-auto">
         <Hero />
         <SocialMedia />
         <Journal />
