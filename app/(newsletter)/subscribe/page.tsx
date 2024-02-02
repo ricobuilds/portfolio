3b69560757c6b79338ad1ec6a1d9a95bc812bb5b
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-02-02 14:18:41 +0000

    updated lipline + styling for sub page

--- app/(newsletter)/subscribe/page.tsx
+++ app/(newsletter)/subscribe/page.tsx
@@ -7,23 +7,23 @@ export default function Subscribe() {
       <div className="relative flex items-center h-screen p-4">
         <div className="flex w-full max-w-5xl mx-auto -mt-10 space-x-0 sm:space-x-8">
           <div className="">
-            <div className="mb-8">
+            <div className="flex justify-center mb-8 sm:justify-start">
               <div className="relative w-24 h-24 overflow-hidden bg-white border rounded-full shadow-md">
                 <Image src={"/headshot.jpeg"} className="absolute inset-0 object-cover w-full h-full" height={200} width={200} alt="" />
               </div>
             </div>
             <div className="w-full mb-8">
-              <h1 className="text-6xl font-semibold">Boundless</h1>
-              <p className="text-2xl">Sharing insights on modern gaming every Someday. To help you build companies, make art, and find your life&apos;s work.</p>
+              <h1 className="text-6xl font-semibold text-center sm:text-left">Boundless</h1>
+              <p className="text-2xl text-center sm:text-left">Sharing insights on modern gaming every Someday. To help you build companies, make art, and find your life&apos;s work.</p>
             </div>
             <div className="w-full mb-10">
-              <div className="w-full max-w-md rounded-lg">
+              <div className="w-full rounded-lg sm:max-w-md">
                 <iframe src="https://embeds.beehiiv.com/3c368bcd-bcd6-4c10-9330-43ca61994c35?slim=true" className="w-full" data-test-id="beehiiv-embed" height="52" frameBorder="0" scrolling="no" style={{ margin: '0', borderRadius: '0px !important', backgroundColor: 'transparent' }}></iframe>
               </div>
             </div>
             <div className="w-full">
               <div className="pt-4">
-                <span className="text-slate-400">I&apos;ll send emails every x time. No spam.</span>
+                <span id="lipline" className="flex justify-center text-slate-400 sm:justify-start">Plus, I send emails every week, never spam or sell your data.</span>
               </div>
             </div>
           </div>
@@ -33,8 +33,8 @@ export default function Subscribe() {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-4 left-4">
-          <div id="copy" className="text-sm">&copy; {new Date().getFullYear()} {name}. All rights reserved.</div>
+        <div className="absolute -translate-x-1/2 bottom-4 sm:left-4 sm:translate-x-0 left-1/2">
+          <div id="copy" className="flex justify-center text-sm sm:justify-start">&copy; {new Date().getFullYear()} {name}. All rights reserved.</div>
         </div>
       </div>
     </main>
