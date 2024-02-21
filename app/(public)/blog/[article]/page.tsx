454f41a3848b774acf8e11d20a4fd44b2dc31aa2
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-02-21 15:40:44 +0000

    x

--- app/(public)/blog/[article]/page.tsx
+++ app/(public)/blog/[article]/page.tsx
@@ -65,7 +65,7 @@ const Page = ({ params }: { params: { article: string } }) => {
   if (!article) notFound();
 
   return (
-    <section className="flex flex-col flex-1 min-h-screen">
+    <section className="flex flex-col flex-1 min-h-screen px-4">
       <article className={
         cn(
           'flex flex-col gap-1 w-full max-w-[696px] mt-20 mx-auto relative',
@@ -92,6 +92,29 @@ const Page = ({ params }: { params: { article: string } }) => {
           <Mdx code={article.body.code} />
         </div>
       </article>
+      <div id="article-nav" className="flex justify-between w-full max-w-[696px] mx-auto py-10 items-center gap-4 mt-16 border-t-[1px] border-border">
+        <div id="prev-item" className="flex items-center gap-2 select-none">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20 " viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7" /><path d="M19 12H5" /></svg>
+          <span>Prev</span>
+        </div>
+        <div className=""></div>
+        <div id="next-item" className="flex items-center gap-2 select-none">
+          <span>Next</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-right"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
+        </div>
+      </div>
+      <div id="newsletter" className="w-full max-w-[696px] mx-auto mb-16">
+        <p className="mt-6 text-2xl italic font-bold">Metasyde Newsletter</p>
+        <p className="mb-8">Get notified when I write something about top news & insights on AI Gaming and the Metaverse, or launch a new project right in your inbox.</p>
+        <div className="w-full rounded-lg">
+          <iframe src="https://embeds.beehiiv.com/3c368bcd-bcd6-4c10-9330-43ca61994c35?slim=true" className="w-full" data-test-id="beehiiv-embed" height="52" frameBorder="0" scrolling="no" style={{ margin: '0', borderRadius: '0px !important', backgroundColor: 'transparent' }}></iframe>
+        </div>
+        <div className="w-full">
+          <div className="pt-4">
+            <span id="lipline" className="flex text-sm text-slate-400">Ps: I send emails every week, never spam or sell your data.</span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 };
