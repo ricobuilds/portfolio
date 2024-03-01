2cab2a207cea437b3230ffd72a959f592af58356
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-03-01 23:33:46 +0000

    added _id as key for /topics

--- app/(public)/topics/page.tsx
+++ app/(public)/topics/page.tsx
@@ -34,7 +34,7 @@ export default async function Topics() {
             <div className="grid gap-6 md:grid-cols-2">
               {
                 topics.map((topic) => (
-                  <Link href={`/topics/${topic.slug}`}>
+                  <Link key={topic._id} href={`/topics/${topic.slug}`}>
                     <div id={topic._id} className="flex items-center gap-3 p-4 border">
                       <div className="text-amethyst-500">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-up-right"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
