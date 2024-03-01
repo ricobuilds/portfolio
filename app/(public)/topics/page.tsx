ce6e03c64313ec0deba435e806c01d603d6adb01
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-03-01 14:44:53 +0000

    created /topics homepage

--- app/(public)/topics/page.tsx
+++ app/(public)/topics/page.tsx
@@ -3,6 +3,8 @@ import { baseWidth } from "@/lib/config"
 import { sanityQuery } from "@/lib/sanity/client"
 import { getTopics } from "@/lib/sanity/queries"
 import { cn } from "@/lib/shared-utils"
+import { Metadata } from "next"
+import Link from "next/link"
 
 async function getAllTopics() {
   const tags = await sanityQuery(getTopics)
@@ -17,15 +19,35 @@ async function getAllTopics() {
   })
 }
 
+export const metadata: Metadata = {
+  title: "Topics"
+}
+
 export default async function Topics() {
   const topics: Topic[] = await getAllTopics()
-  console.log(topics)
   return (
     <>
       <main className="w-full px-4">
         <div className={cn(baseWidth, "min-h-screen w-full mx-auto")}>
           <div className="relative flex flex-col w-full gap-10 pt-20">
             <h1 className="text-6xl font-semibold">Topics</h1>
+            <div className="grid gap-6 md:grid-cols-2">
+              {
+                topics.map((topic) => (
+                  <Link href={`/topics/${topic.slug}`}>
+                    <div id={topic._id} className="flex items-center gap-3 p-4 border">
+                      <div className="text-amethyst-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-up-right"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
+                      </div>
+                      <div className="w-full">
+                        <p className="text-lg font-semibold text-amethyst-500"> {topic.title}</p>
+                        <p className="text-slate-500"> {topic?.description ?? "Lorem Ipseum and them man"}</p>
+                      </div>
+                    </div>
+                  </Link>
+                ))
+              }
+            </div>
           </div>
         </div>
       </main>
--- app/_hidden/glossary/page.tsx
+++ app/_hidden/glossary/page.tsx
@@ -1,3 +1,5 @@
+import { baseWidth } from "@/lib/config"
+import { cn } from "@/lib/shared-utils"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -6,8 +8,14 @@ export const metadata: Metadata = {
 
 export default function Glossary() {
   return (
-    <main>
-      <h1>Glossary</h1>
-    </main>
+    <>
+      <main className="w-full px-4">
+        <div className={cn(baseWidth, "min-h-screen w-full mx-auto")}>
+          <div className="relative flex flex-col w-full gap-10 pt-20">
+            <h1 className="text-6xl font-semibold">Glossary</h1>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
