09938ca91649fd74f8ad46e790b8f5f7319cf624
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-03-04 09:34:09 +0000

    updated glossary metadata

--- app/_hidden/glossary/page.tsx
+++ app/_hidden/glossary/page.tsx
@@ -2,8 +2,12 @@ import { baseWidth } from "@/lib/config"
 import { cn } from "@/lib/shared-utils"
 import { Metadata } from "next"
 
+const title = "AI Gaming & Metaverse Glossary"
+const description = ""
+
 export const metadata: Metadata = {
-  title: "Glossary"
+  title: title,
+  description: description
 }
 
 export default function Glossary() {
@@ -12,7 +16,7 @@ export default function Glossary() {
       <main className="w-full px-6">
         <div className={cn(baseWidth, "min-h-screen w-full mx-auto")}>
           <div className="relative flex flex-col w-full gap-10 pt-20">
-            <h1 className="text-6xl font-semibold">Glossary</h1>
+            <h1 className="text-6xl font-semibold">{title}</h1>
           </div>
         </div>
       </main>
