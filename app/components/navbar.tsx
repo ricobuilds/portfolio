0eb20d0038d9c2f257741d5ecdbb4e8fb7f8c55f
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-03-07 08:58:23 +0000

    fixed navbar on mobile

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <nav className={cn(baseWidth, "flex justify-between w-full py-5 items-center")}>
         <div className="flex items-center gap-4">
           <Image src={'/ricobuilds.png'} alt="Enric Trillo Cryptopunk" width={64} height={64} className="inline w-8 h-8 transition-all duration-300 rounded-full" />
-          <ul className="flex gap-2">
+          <ul className="flex flex-wrap gap-2">
             {nav.map((i, idx) => (
               <li key={idx}>
                 <Link className="px-2 py-2 rounded-md hover:bg-slate-100" href={i.route}>{i.label}</Link>
