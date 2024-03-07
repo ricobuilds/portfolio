0515342f5a38cf2bc17781f917b5c3f4aac97311
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-03-07 09:11:57 +0000

    testing fix on /about heading

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -148,9 +148,11 @@ export default function About() {
         <div className={cn(baseWidth, "w-full mx-auto")}>
           <div className="relative flex flex-col gap-10 pt-20">
             <Image src={'/headshot.jpeg'} alt="Enric Trillo" width={600} height={600} className="inline w-24 h-24 mx-auto transition-all duration-300 rounded-full ring-2 ring-slate-200/80 hover:ring-4" />
-            <Balancer as={"h1"} className={cn(kanit.className, "text-3xl md:text-5xl lg:text-7xl text-center")}>
-              Hey, I&apos;m <span className="text-amethyst-500">Enric Trillo</span>, a fullstack developer building the future of gaming.
-            </Balancer>
+            <h1 className="mx-auto">
+              <Balancer className={cn(kanit.className, "text-3xl md:text-5xl lg:text-7xl text-center")}>
+                Hey, I&apos;m <span className="text-amethyst-500">Enric Trillo</span>, a fullstack developer building the future of gaming.
+              </Balancer>
+            </h1>
             <div id="content" className="flex flex-col gap-8 max-w-[696px] mx-auto">
               {intro()}
               {mission()}
