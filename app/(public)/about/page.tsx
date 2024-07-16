488d42b4674378afab0f638ffd2affbebf91918c
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-07-16 13:47:33 +0100

    updated about page content

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,4 +1,6 @@
+import { logos } from "@/app/sections/hero"
 import { StructuredData } from "@/components/structured-data"
+import { Button } from "@/components/ui/button"
 import { baseWidth } from "@/lib/config"
 import { routes } from "@/lib/routes"
 import { cn } from "@/lib/shared-utils"
@@ -76,49 +78,25 @@ const kanit = Kanit({
 
 export default function About() {
 
-  const intro = () => (
-    <section id="intro">
-      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>About me</h2>
+  const bio = () => (
+    <section id="bio">
+      <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Bio</h2>
       <p className="mb-6">
-        I&apos;m a fullstack developer, with 6+ years of development experience, passionate for building the future of gaming using AI and emerging technologies.
-        My journey began as a multi-faceted creative. In 2014, I made a name for myself as a DJ at just 15 years old,
-        setting the stage for my diverse journey. By 16, I was writing my first line of code in Python and going deep into graphic design during my time at St. Francis Xavier College. Over the years, I&apos;ve honed skills in UI/UX design, motion graphics, videography and more.
+        In 2014, I started DJing at 15. A year later, I learned graphic design and coding at <strong>St. Francis Xavier College</strong>. These skills helped me create covers for my mixes and write my first line of Python code at 16.
       </p>
       <p className="mb-6">
-        My journey into the world of AI began during my time at the <strong>University of Northampton</strong>, where I earned a degree in Computing (Graphics & Visualisation) in 2020. The interest for AI sparked from the AI module taught by Professor Mu Mu, where I achieved an A* and later wrote about my experience in a <Link target={"_blank"} href={"https://medium.com/swlh/an-image-classifier-with-keras-2f0e9b868a36"} className="text-amethyst-500">Medium article</Link> featured by The Startup publication.
-      </p>
-      <p>Some of my recent gigs include working as a Fullstack Web3 Developer at CWJ Capital.</p>
-    </section>
-  )
-
-  const mission = () => (
-    <section id="mission">
-      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>My Mission</h2>
-      <p>In 2022,
-        <strong>Metasyde was born</strong>, marking the beginning of my quest to follow my curiosity and redefine gaming with emerging technologies.
-        My mission is to push the boundaries of what&apos;s possible in gaming through cutting-edge AI technologies. I look to evangelise the <span className="italic">boundless mindset</span> too.
-      </p>
-    </section>
-  )
-
-  const vision = () => (
-    <section id="vision">
-      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Metasyde Vision</h2>
-      <p className="mb-6">
-        At Metasyde, we see a future where blockchain seamlessly integrates with gaming to create immersive, intuitive,
-        and dynamic experiences for players. Our goal is to be at the forefront of this evolution, pioneering new ways to engage with games
-        and the metaverse.
+        During my time at the <strong>University of Northampton</strong>, between 2017 and 2020, I experimented with a bunch of skills–journalism, UI/UX, motion design, video editing and videography, even bartending.
       </p>
       <p>
-        We see Web3 Gaming as a critical layer contributing to the overall concept of the &quot;Metaverse&quot;, alongside emerging technologies like AI and NFTs.
+        My love for AI sparked in my third year, which led me to an A* grade, and writing about my experience in a <Link target={"_blank"} href={"https://medium.com/swlh/an-image-classifier-with-keras-2f0e9b868a36"} className="text-amethyst-500">featured Medium article</Link> with The Startup.
       </p>
     </section>
   )
 
-  const topics = () => (
-    <section id="topics">
-      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Topics</h2>
-      <p className="mb-6">I write content about my 8 topics to help you keep up with top news & insights on Web3 Gaming and the Metaverse across my <Link href={routes.journal} className="text-amethyst-500">blog</Link>, <Link href={routes.subscribe} className="text-amethyst-500">newsletter</Link> and social media channels:</p>
+  const media = () => (
+    <section id="media">
+      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Media</h2>
+      <p className="mb-6">Aside from being a developer doing <i>developer</i> things, I publish content about disruptive technologies across my <Link href={routes.journal} className="text-amethyst-500">blog</Link>, <Link href={routes.subscribe} className="text-amethyst-500">email</Link> and media channels:</p>
       <ul className="mb-6 ml-8 list-disc">
         <li>
           <Link href={routes.twitter} className="text-amethyst-500">Twitter</Link>
@@ -130,13 +108,14 @@ export default function About() {
           <Link href={routes.youtube} className="text-amethyst-500">YouTube</Link>
         </li>
       </ul>
-      <p>The 8 main topics I discuss online include: AR + VR, Artificial Intelligence, Blockchain, Gaming, Haptic technology, Metaverse, Robotics and Web.</p>
+      <p>The tech topics I like to talk about include: AR and VR, Artificial Intelligence, Blockchain, Gaming, Haptic technology, Metaverse, Robotics and Web.</p>
     </section>
   )
+
   const journey = () => (
     <section id="journey">
-      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Follow the Journey</h2>
-      <p>I&apos;m on a mission to shape the future of Web3 Gaming and the Metaverse. Whether you&apos;re a gamer, fellow developer or simply curious about Web3 in gaming, I&apos;d love to connect and collaborate.</p>
+      <h2 className={cn(kanit.className, "font-semibold text-xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Journey</h2>
+      <p>I&apos;m on a mission to learn new skills and disruptive tech. For this, I need to upgrade my current skillset by going back into my ways of being a multi-faceted creative, and develop skills across tech disciplines – this is how I plan to cultivate a multi-disciplinary skillset.</p>
     </section>
   )
   return (
@@ -145,20 +124,44 @@ export default function About() {
       <StructuredData data={aboutBreadcrumbSchema} />
       <main className="w-full px-6">
         <div className={cn(baseWidth, "w-full mx-auto")}>
-          <div className="relative flex flex-col gap-10 pt-20">
-            <Image src={'/headshot.jpeg'} alt="Enric Trillo" width={600} height={600} className="inline w-24 h-24 mx-auto transition-all duration-300 rounded-full ring-2 ring-slate-200/80 hover:ring-4" />
-            <h1 className="mx-auto">
-              <Balancer className={cn(kanit.className, "text-2xl md:text-5xl lg:text-7xl text-center")}>
-                Hey, I&apos;m <span className="text-amethyst-500">Enric Trillo</span>, a fullstack developer building the future of gaming.
-              </Balancer>
-            </h1>
-            <div id="content" className="flex flex-col gap-8 max-w-[680px] mx-auto">
-              {intro()}
-              {mission()}
-              {vision()}
-              {topics()}
+          <div className="relative flex flex-col items-center gap-6 pt-20">
+
+            <div id="content" className="flex flex-col gap-8 max-w-[560px] mx-auto">
+              <div className="flex flex-col items-center max-w-[560px] gap-4">
+                <Image src={'/headshot.jpeg'} alt="Enric Trillo" width={600} height={600} className="inline w-24 h-24 mx-auto transition-all duration-300 rounded-full ring-2 ring-slate-200/80 hover:ring-4" />
+                <h1 className="text-lg font-bold">Enric Trillo</h1>
+                <p className="text-slate-600">Fullstack & NoCode Developer</p>
+                <p>
+                  I&apos;m Enric, born & raised in southern Madrid 🇪🇸, I&apos;ve been living in London 🇬🇧 since my early teens, and I&apos;m from a small hispanic nation in Central Africa few can mark on the map – Equatorial Guinea 🇬🇶.
+                </p>
+                <div id="socials" className="flex items-center gap-6 mt-4">
+                  <Link href={routes.twitter} target={"_blank"}>
+                    <div id="𝕏">
+                      <span className="text-xl">𝕏</span>
+                    </div>
+                  </Link>
+                  <Link href={routes.linkedin} target={"_blank"}>
+                    <div id="linkedin">
+                      {logos.linkedin}
+                    </div>
+                  </Link>
+                  <Link href={routes.youtube} target={"_blank"}>
+                    <div id="youtube">
+                      {logos.youtube}
+                    </div>
+                  </Link>
+                  <Link href={routes.github} target={"_blank"}>
+                    <div id="github">
+                      {logos.github}
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              {bio()}
+              {media()}
               {journey()}
-              <p>Let&apos;s create the next level of gaming together.</p>
+              <p>Reach out to collaborate.</p>
+              <Link href={routes.email} className="w-full px-4 py-2 text-center text-white rounded-md bg-charkol">hola@enrictrillo.com</Link>
             </div>
           </div>
         </div>
