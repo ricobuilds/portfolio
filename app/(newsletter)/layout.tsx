196504a82c91e73d646bbcf8c0672a77c0144924
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-07-17 10:49:43 +0100

    updated newsletter layout

--- app/(newsletter)/layout.tsx
+++ app/(newsletter)/layout.tsx
@@ -1,49 +1,6 @@
-import { siteMetadata } from "@/lib/site.metadata"
-import { GeistSans } from "geist/font/sans"
-import { Metadata } from "next"
 import "../globals.css";
+import { GeistSans } from "geist/font/sans"
 import { UmamiScript } from "../../components/umami-script";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(siteMetadata.siteUrl),
-  title: {
-    default: siteMetadata.default,
-    template: `%s - ${siteMetadata.title}`
-  },
-  description: siteMetadata.description,
-  alternates: {
-    canonical: siteMetadata.siteUrl
-  },
-  openGraph: {
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    url: siteMetadata.siteUrl,
-    siteName: siteMetadata.title,
-    locale: 'en_GB',
-    type: 'website',
-    images: `/base-og.png`,
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    creator: '@ricobuilds',
-    site: siteMetadata.siteUrl,
-    images: `/base-og.png`,
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-}
 
 export default function RootLayout({
   children,
