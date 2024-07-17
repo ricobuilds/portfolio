2e3117cacc66f8fa92843af74a540f32255085e5
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-07-17 11:57:25 +0100

    udpated desc and kws in siteMetadata

--- lib/site.metadata.ts
+++ lib/site.metadata.ts
@@ -1,11 +1,11 @@
 export const siteMetadata = {
   default: "Enric Trillo － Fullstack Developer",
   title: "Enric Trillo",
-  description: "Hi, I'm Enric Trillo, a fullstack & nocode developer building with disruptive technologies, and sharing insights from the journey to help you thrive in a future world driven by AI.",
+  description: "Hi, I'm Enric Trillo, a fullstack & nocode developer focused on building with disruptive technologies. Join me on the journey of upskilling and innovating to thrive in a future world driven by AI.",
   siteUrl: "https://enrictrillo.com",
   twitter: "https://x.com/ricobuilds",
   locale: "en-GB",
   language: "en-gb",
   baseUrlImage: "/base-og.png",
-  keywword: "ricobuilds, enrict trillo, fullstack development, learn machine learning, learn deep learning, learn typescript, learn nextjs, learn python"
+  keywword: "ricobuilds, Enric Trillo, fullstack development, AI development, machine learning, Typescript, Next.js, Python"
 }
