7b37f92469f45ae0245b57824a78825bdfc23761
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-07-17 10:28:24 +0100

    updated sitemetadata

--- lib/site.metadata.ts
+++ lib/site.metadata.ts
@@ -1,9 +1,10 @@
 export const siteMetadata = {
   default: "Enric Trillo － Fullstack Developer",
   title: "Enric Trillo",
-  description: "Hi, I'm Enric Trillo, a fullstack & nocode developer building with disruptive technologies, and sharing insights from the journey.",
+  description: "Hi, I'm Enric Trillo, a fullstack & nocode developer building with disruptive technologies, and sharing insights from the journey to help you thrive in a future world driven by AI.",
   siteUrl: "https://enrictrillo.com",
   twitter: "https://x.com/ricobuilds",
   locale: "en-GB",
+  language: "en-gb",
   baseUrlImage: "/base-og.png"
 }
