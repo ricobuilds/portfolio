349b85d37076bf1c3a7c1b388b99134558ba3952
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-07-21 09:36:01 +0100

    created blog-card to avoid duplication

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -5,6 +5,7 @@ import { MDXArticle } from "@/app/types/Article"
 import Image from "next/image"
 import { formatTag, getAllPosts } from "@/lib/mdx"
 import { generateMetadata } from "@/lib/seo"
+import { BlogCard } from "@/components/blog-card"
 
 export const metadata = generateMetadata({
   title: 'Blog',
@@ -30,25 +31,7 @@ export default async function Blog() {
                   {
                     posts.map((post) => (
                       <li key={post.slug}>
-                        <div className="flex flex-col w-full gap-3 outline-none group">
-                          <Link href={`/blog/${post.slug}`} className="w-full overflow-hidden duration-300 ring-0 rounded-2xl">
-                            <Image src={"/blog-og.png"} height={1200} width={630} alt={post.title} loading="lazy" className="object-cover w-full transition-all duration-300 ease-in-out rounded-2xl group-hover:scale-[1.03]" />
-                          </Link>
-                          <div className="flex flex-col gap-2 px-2">
-                            <div className="flex gap-2">
-                              {post.tags && post.tags.map((t, idx) => (
-                                <Link key={idx} href={`/tags/${formatTag(t)}`} className="text-[10px] w-fit uppercase underline text-amethyst-500">
-                                  {t}
-                                </Link>
-                              ))}
-                            </div>
-                            <Link href={`/blog/${post.slug}`}>
-                              <h3 className="font-sans font-semibold">{post.title}</h3>
-                            </Link>
-                            <p className="text-sm line-clamp-1">{post.description}</p>
-                            <p className="uppercase text-[10px]">By <span className="text-slate-500">{post.author}</span>  / <span>{convertDate(post.date, { day: "2-digit", month: "long", year: "numeric" })}</span></p>
-                          </div>
-                        </div>
+                        <BlogCard post={post} />
                       </li>
                     ))
                   }
--- app/sections/blog.tsx
+++ app/sections/blog.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import Link from "next/link"
 import { MDXArticle } from "../types/Article"
-import { cn, convertDate } from "@/lib/shared-utils"
+import { cn } from "@/lib/shared-utils"
 import { Kanit } from "next/font/google"
-import Image from "next/image"
 import { routes } from "@/lib/routes"
-import { formatTag, getAllPosts } from "@/lib/mdx"
+import { getAllPosts } from "@/lib/mdx"
+import { BlogCard } from "@/components/blog-card"
 
 const kanit = Kanit({
   weight: "800",
@@ -21,25 +21,7 @@ const PostsList = ({ posts }: { posts: MDXArticle[] }) => {
     <ul className="grid w-full gap-8 mt-10 md:grid-cols-6">
       {slicedPostList.map((post, idx) => (
         <li key={idx} className={`${idx < 2 ? 'md:col-span-3' : 'col-span-2'}`}>
-          <div className="flex flex-col w-full gap-3 outline-none group">
-            <Link href={`/blog/${post.slug}`} className="w-full overflow-hidden duration-300 ring-0 rounded-2xl">
-              <Image src={"/blog-og.png"} height={1200} width={630} alt={post.title} loading="lazy" className="object-cover w-full transition-all duration-300 ease-in-out rounded-2xl group-hover:scale-[1.03]" />
-            </Link>
-            <div className="flex flex-col gap-2 px-2">
-              <div className="flex gap-2">
-                {post.tags && post.tags.map((t, idx) => (
-                  <Link key={idx} href={`/tags/${formatTag(t)}`} className="text-[10px] w-fit uppercase underline text-amethyst-500">
-                    {t}
-                  </Link>
-                ))}
-              </div>
-              <Link href={`/blog/${post.slug}`}>
-                <h3 className="font-sans font-semibold">{post.title}</h3>
-              </Link>
-              <p className="text-sm line-clamp-1">{post.description}</p>
-              <p className="uppercase text-[10px]">By <span className="text-slate-500">{post.author}</span>  / <span>{convertDate(post.date, { day: "2-digit", month: "long", year: "numeric" })}</span></p>
-            </div>
-          </div>
+          <BlogCard post={post} />
         </li>
       ))}
     </ul>
