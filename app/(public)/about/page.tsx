c2dcba73edd9537dc606a2a36dd5ef734feb41fd
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-08-10 23:45:48 +0100

    update: added grad photo

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -23,9 +23,9 @@ const kanit = Kanit({
 
 export default function About() {
 
-  const bio = () => (
-    <section id="bio">
-      <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Bio</h2>
+  const story = () => (
+    <section id="story">
+      <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Story</h2>
       <p className="mb-6">
         In 2014, I started DJing at 15. A year later, I learned graphic design and coding at <strong>St. Francis Xavier College</strong>. These skills helped me create covers for my mixes and write my first line of Python code at 16.
       </p>
@@ -74,53 +74,124 @@ export default function About() {
             <div id="content" className="flex flex-col gap-8 max-w-[560px] mx-auto">
               <div className="flex flex-col items-center max-w-[560px] gap-4">
                 <Image src={'/headshot.jpeg'} alt="Enric Trillo" width={600} height={600} className="inline w-24 h-24 mx-auto transition-all duration-300 rounded-full ring-2 ring-slate-200/80 hover:ring-4" />
-                <h1 className="text-lg font-bold">Enric Trillo</h1>
+                <h1 className="text-lg font-bold">About Enric Trillo</h1>
                 <p className="text-slate-600">Fullstack & AI Agents Developer</p>
-                <p>
-                  I&apos;m Enric, born & raised in southern Madrid 🇪🇸, I&apos;ve been living in London 🇬🇧 since my early teens, and I&apos;m from a small hispanic nation in Central Africa few can mark on the map – Equatorial Guinea 🇬🇶.
-                </p>
-                <div id="socials" className="flex items-center gap-6 mt-4">
-                  <Link href={routes.twitter} target={"_blank"}>
-                    <div id="𝕏">
-                      <span className="text-xl">𝕏</span>
-                    </div>
-                  </Link>
-                  <Link href={routes.linkedin} target={"_blank"}>
-                    <div id="linkedin">
-                      {logos.linkedin}
-                    </div>
-                  </Link>
-                  <Link href={routes.youtube} target={"_blank"}>
-                    <div id="youtube">
-                      {logos.youtube}
-                    </div>
-                  </Link>
-                  <Link href={routes.github} target={"_blank"}>
-                    <div id="github">
-                      {logos.github}
-                    </div>
-                  </Link>
-                  <Link href={routes.rss} target={"_blank"}>
-                    <div id="rss">
-                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-rss"><path d="M4 11a9 9 0 0 1 9 9" /><path d="M4 4a16 16 0 0 1 16 16" /><circle cx="5" cy="19" r="1" /></svg>
-                      </span>
-                    </div>
-                  </Link>
-                </div>
-                <div className="flex flex-col gap-3 p-4 border rounded-xl">
-                  <h2 className="font-bold">Shift Forward Newsletter</h2>
-                  <p className="text-slate-500">Sharing the latest insights and developments in the world of disruptive technologies to help you thrive in a future driven by AI.</p>
-                  <div className="flex items-center gap-2">
-                    <BeehiivCustom />
-                  </div>
-                </div>
+                <section id="intro" className="space-y-6">
+                  <p>
+                    Hey! I&apos;m Enric J Trillo Nchana, a fullstack developer with a passion AI and multi-agent systems. As a previous digital developer and hybrid tech sales rep, I now focus on disruptive technologies and multi agent systems.
+                  </p>
+                  <p>
+                    I was born and raised in southern Madrid 🇪🇸, I&apos;ve been living in London 🇬🇧 since my early teens, and I&apos;m from a small hispanic nation in Central Africa few can mark on the map – Equatorial Guinea 🇬🇶.
+                  </p>
+                </section>
               </div>
-              {bio()}
-              {media()}
-              {journey()}
-              <p>Reach out to collaborate.</p>
-              <Link href={routes.email} className="w-full px-4 py-2 text-center text-white rounded-md bg-charkol">→ hola@enrictrillo.com</Link>
+              <section id="story" className="space-y-6">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Story</h2>
+                <p>
+                  In 2014, at just 15, I started DJing–a craft that taught me the importance of rhythm, timing, and creativity. A year later, I dove into graphic design and coding at <Link target="_blank" className="font-bold underline text-amethyst-500" href="https://www.sfx.ac.uk/">St. Francis Xavier College</Link>, creating cover art for my mixes and writing my first line of Python code at 16. These early experiences laid the foundation for my journey in tech.
+                </p>
+                <div className="object-contain w-full overflow-hidden">
+                  <Image priority src="/graduation.jpg" width={600} height={600} alt="Enric Trillo, graduation photo" />
+                </div>
+                <p>
+                  During my years at the <Link target="_blank" className="font-bold underline text-amethyst-500" href="https://www.northampton.ac.uk/">University of Northampton</Link> (2017-2020), I didn&apos;t just stick to one thing. I explored everything from journalism to UI/UX design, motion graphics, video editing, and even bartending. But it was in my third year that I discovered my true passion: Artificial Intelligence. This revelation led to an A* grade and the publication of a featured Medium article with The Startup, where I shared my process and insights on AI.
+                </p>
+              </section>
+              <section id="mission">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Mission</h2>
+                <p>Today, I&apos;m on a mission to build disruptive tech and assemble AI multi-agent systems that can transform industries. I&apos;m continuously learning new skills across tech disciplines to stay ahead in this rapidly evolving landscape.</p>
+              </section>
+              <section id="what-i-do" className="space-y-6">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>What I Do</h2>
+                <p>I run the <i>Shift Forward Newsletter</i>. This is where I share weekly actionable insights to help others thrive in this AI-driven world. My online presence spans across various platforms, including:</p>
+                <ul className="mb-0 ml-4">
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>Twitter</strong>: Sharing thoughts on the latest in tech and AI.
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>LinkedIn</strong>: Professional updates and industry discussions.
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>YouTube</strong>: Video content on disruptive technologies and tutorials.
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>Blog</strong>: Deep dives into tech topics like AR/VR, AI and multi agent systems, blockchain, gaming, haptics, the metaverse, robotics, and more.
+                  </li>
+                </ul>
+              </section>
+              <section id="certification">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Awards & Recognition</h2>
+                <ul className="mb-0 ml-8 list-disc">
+                  <li>
+                    <strong>Featured in Medium&apos;s The Startup</strong>: For my <Link className="underline text-amethyst-500" href="https://medium.com/swlh/an-image-classifier-with-keras-2f0e9b868a36">article</Link> on AI and my academic journey.
+                  </li>
+                  <li>
+                    <strong>University of Northampton</strong>: Achieved an A* grade in AI during my final year.
+                  </li>
+                  <li>
+                    <strong>Certifications:</strong>
+                    <ul className="mb-0 ml-4">
+                      <li className="before:content-['→'] before:mr-2">
+                        <strong>Fundamentals of Digital Marketing</strong>, Google – <Link target="_blank" href={"https://skillshop.exceedlms.com/student/collection/654330-digital-marketing?sid=0339c211-017a-43b7-9485-6c24e997a4aa&sid_i=1" + "?ref=enrictrillo"} className="underline text-amethyst-500 underline-offset-2">course</Link>
+                      </li>
+                      <li className="before:content-['→'] before:mr-2">
+                        <strong>Keyword Research with Semrush</strong>, Semrush – <Link target="_blank" href={"https://www.semrush.com/academy/courses/keyword-research-with-semrush-step-by-step-guide/" + "?ref=enrictrillo"} className="underline text-amethyst-500 underline-offset-2">course</Link>
+                      </li>
+                      <li className="before:content-['→'] before:mr-2">
+                        <strong>Build LLM Apps with LangChain.js</strong>, DeepLearning.AI – <Link target="_blank" href={"https://www.deeplearning.ai/short-courses/build-llm-apps-with-langchain-js/" + "?ref=enrictrillo"} className="underline text-amethyst-500 underline-offset-2">course</Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </section>
+              <section id="testimonials">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Testimonials</h2>
+                <div className="grid grid-cols-1 gap-3">
+                  <blockquote className="relative max-w-2xl p-6 mx-auto bg-white border rounded-lg">
+                    <div title="Testimonial" className="mb-4 italic text-gray-600">
+                      "Your app is bomb! Thank you as it helps tremendously with my workflow..."
+                    </div>
+                    <div title="Testimonial Author" className="font-semibold text-gray-800">
+                      – Jordan (YouTube comment)
+                    </div>
+                  </blockquote>
+                  <blockquote className="relative max-w-2xl p-6 mx-auto bg-white border rounded-lg">
+                    <div title="Testimonial" className="mb-4 italic text-gray-600">
+                      "Very cool, a medicine to my paranoia that chatgpt will one day have an absurd paywall and all of my conversations would get lost"
+                    </div>
+                    <div title="Testimonial Author" className="font-semibold text-gray-800">
+                      – Amitay Gilboa (Product Hunt comment)
+                    </div>
+                  </blockquote>
+                </div>
+              </section>
+              <section id="opportunities" className="space-y-6">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Opportunities</h2>
+                <p>
+                  I&apos;m open to collaborations and new opportunities. Whether you&apos;re interested in working on a project together, need some insights on multi AI agent systems, or just want to chat about disruptive tech, feel free to reach out.
+                </p>
+                <ul className="mb-6 ml-4">
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>Email:</strong> <Link href={routes.email} className="underline text-amethyst-500 underline-offset-2">hola@enrictrillo.com</Link>
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>Twitter:</strong> <Link href={routes.twitter} className="underline text-amethyst-500 underline-offset-2">@ricobuilds</Link>
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>LinkedIn:</strong> <Link href={routes.linkedin} className="underline text-amethyst-500 underline-offset-2">linkedin.com/in/enrictrillo</Link>
+                  </li>
+                  <li className="before:content-['→'] before:mr-2">
+                    <strong>YouTube:</strong> <Link href={routes.youtube} className="underline text-amethyst-500 underline-offset-2">Enric Trillo @ricobuilds</Link>
+                  </li>
+                </ul>
+              </section>
+              <section id="cta">
+                <h2 className={cn(kanit.className, "font-semibold text-lg uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Shift Forward</h2>
+                <p className="mb-6">Join <strong>Shift Forward</strong> for weekly actionable insights on disruptive tech, like Web3, Extended Reality & Robotics, to thrive in a future world driven by AI.</p>
+                <div className="flex items-center gap-2">
+                  <BeehiivCustom />
+                </div>
+              </section>
             </div>
           </div>
         </div>
