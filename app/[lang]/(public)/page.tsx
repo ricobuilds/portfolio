44384ba0d3901f07e7dc816f7683eeeff287c8cc
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-02 20:36:54 +0100

    wip

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -17,6 +17,7 @@ import { MDXArticle } from "@/app/types/Article";
 import { getAllPosts } from "@/lib/mdx";
 import { BlogCard } from "@/components/blog-card";
 import { routes } from "@/lib/routes";
+import { BeehiivCustom } from "@/components/beehiiv-custom";
 
 const kanit = Kanit({
   subsets: ['latin'],
@@ -78,10 +79,6 @@ const homeSchema: WithContext<Person> = {
     "https://github.com/ricobuilds"
   ]
 }
-
-const DynamicNewsletter = dynamic(() => import("@/sections/newsletter"), {
-  loading: () => <p>Loading...</p>,
-})
 
 export default async function Home({ params }: { params: { lang: Locale } }) {
   const tl = await getDictionary(params.lang);
@@ -181,8 +178,26 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
             </section >
             {/* <Skills/> */}
             <section id="faq" className="flex flex-col py-16"></section>
+            <section id="cta" className="flex flex-col py-16"></section>
             <Work />
-            <DynamicNewsletter />
+            <section id="shift-forward" className="flex flex-col py-16">
+              <h2 className={cn(kanit.className, "flex items-center  text-center text-2xl font-medium px-4 py-1 mb-3 mx-auto text-white uppercase w-fit bg-amethyst-500")}>
+                Shift Forward Newsletter
+              </h2>
+              <div className="w-full max-w-3xl mx-auto">
+                <h3 className={cn("flex items-center mx-auto mb-3 text-lg font-medium text-center w-fit")}>
+                  <Balancer>Get notified when I push out top alpha on disruptive technologies, or launch a new Metasyde project right in your inbox.</Balancer>
+                </h3>
+                <div className="flex max-w-lg mx-auto mt-4">
+                  <BeehiivCustom />
+                </div>
+                <div className="w-full mt-3 text-center">
+                  <div className="pb-4">
+                    <span id="lipline" className="text-xs text-slate-400">Ps: I send emails every week, never spam or sell your data.</span>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </main>
