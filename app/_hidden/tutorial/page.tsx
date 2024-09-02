59674ccca65ab6a229af390583f55a544d14aa85
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-02 13:06:29 +0100

    update: tutorials page metadata

--- app/_hidden/tutorial/page.tsx
+++ app/_hidden/tutorial/page.tsx
@@ -2,7 +2,7 @@ import { generateMetadata } from "@/lib/seo"
 
 export const metadata = generateMetadata({
   title: "Explore our Tutorials",
-  description: "The disruptive technology tutorials you need to thrive in a world with AI. Tools covered include Python, PyTorch, CrewAI and more."
+  description: "Learn the latest disruptive technologies you need to thrive in a world with AI, including Generative AI, RAG, Deep Learning, Python, PyTorch, CrewAI, NextJS 14, and more."
 })
 
 export default function Page() {
