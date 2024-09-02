d8ef4816e9036126d4f26a233f7fbe64a1a2bfe0
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-02 12:48:10 +0100

    update: testimonials page metadata

--- app/_hidden/testimonials/page.tsx
+++ app/_hidden/testimonials/page.tsx
@@ -1,3 +1,9 @@
+import { generateMetadata } from "@/lib/seo"
+
+export const metadata = generateMetadata({
+  title: "Testimonials"
+})
+
 export default function Testimonials() {
   return (
     <div>
