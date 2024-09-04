56e307768d5aeac9b431d53b5eac8e74a8cae455
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-04 13:00:57 +0100

    update: login styles and thicka border in buttons

--- app/[lang]/components/navbar.tsx
+++ app/[lang]/components/navbar.tsx
@@ -58,9 +58,9 @@ const Navbar = ({
         </ul>
         {/* <div className="w-full mx-2 transition-colors duration-300 border-t border-amethyst-500 group-hover:border-green-400"></div> */}
         <div className="items-center hidden gap-2 md:flex shrink-0">
-          <button className="flex items-center px-3 py-2 text-sm border rounded-full border-obsidian-300 hover:bg-obsidian-100">Log In</button>
+          <button className="flex items-center px-3 py-2 text-sm border-2 border-black hover:bg-black hover:text-white">Log In</button>
           <Link href={routes.subscribe} className="hidden md:flex">
-            <button className="flex items-center px-3 py-2 text-sm text-white rounded-full bg-amethyst-500">Join Shift Forward</button>
+            <button className="flex items-center px-3 py-2 text-sm text-white border-2 border-transparent hover:border-black bg-amethyst-500">Join Shift Forward</button>
           </Link>
           <LanguageSwitcher />
         </div>
