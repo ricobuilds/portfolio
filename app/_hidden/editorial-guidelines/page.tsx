d9466022941f60361966dbf7207cdb99a37de28e
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-04 19:03:55 +0100

    update: ediotrialguilines page

---
+++ app/_hidden/editorial-guidelines/page.tsx
@@ -0,0 +1,11 @@
+import { getDictionary } from "@/app/[lang]/dictionaries"
+import { Locale } from "@/constants/i18n.config"
+
+export default async function EditorialGuidelines({ params }: { params: { lang: Locale } }) {
+  const tl = await getDictionary(params.lang)
+  return (
+    <div>
+      <h1>Hello from Editorial Guidelines</h1>
+    </div>
+  )
+}
