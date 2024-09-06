0ffffbf0414b00ec2335fab17bed2a4ae5bd810a
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-06 12:39:07 +0100

    update: fix navbar clash

--- app/[lang]/components/navbar.tsx
+++ app/[lang]/components/navbar.tsx
@@ -62,66 +62,67 @@ const Navbar = ({
 
   return (
     <header className="sticky top-0 z-50 flex flex-col justify-center w-full bg-white border-b h-18">
-      <div id="mainnav" className={cn("max-w-[1360px]", "flex justify-between w-full py-5 px-6 items-center mx-auto")}>
+      <div id="mainnav" className={cn("max-w-[1360px]", "flex w-full py-5 px-6 items-center mx-auto")}>
         <div className="flex items-center gap-4 shrink-0">
           <Image src={'/images/ricobuilds.png'} alt="Enric Trillo Cryptopunk" width={64} height={64} className="inline w-8 h-8 transition-all duration-300 rounded-full" />
         </div>
         {/* <div className="w-full mx-2 transition-colors duration-300 border-t border-amethyst-500 group-hover:border-green-400"></div> */}
-        <ul className="absolute flex-wrap hidden gap-1 text-sm -translate-x-1/2 md:flex left-1/2">
-          <NavigationMenu>
-            <NavigationMenuList>
-              {
-                tl['menu'].map((item, idx) => {
-                  return item.type === "single" ? (
-                    <NavigationMenuItem key={idx}>
-                      <Link href={item.link as string} legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                          {item.label}
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                  ) : (
-                    <NavigationMenuItem key={idx}>
+        <div id="right-section" className="flex justify-end flex-1 gap-20">
+          <ul className="flex-wrap hidden gap-1 text-sm lg:flex">
+            <NavigationMenu>
+              <NavigationMenuList>
+                {
+                  tl['menu'].map((item, idx) => {
+                    return item.type === "single" ? (
+                      <NavigationMenuItem key={idx}>
+                        <Link href={item.link as string} legacyBehavior passHref>
+                          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            {item.label}
+                          </NavigationMenuLink>
+                        </Link>
+                      </NavigationMenuItem>
+                    ) : (
+                      <NavigationMenuItem key={idx}>
                         <NavigationMenuTrigger>{item.label}</NavigationMenuTrigger>
-                      <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                          {Array(6).fill({
-                            title: "Coming Soon",
-                            href: "#",
-                            description:
-                              "Keep an eye out and come back later. Big things coming soon.",
-                          }).map((component) => (
-                            <NavigationSubItem
-                              key={component.title}
-                              title={component.title}
-                              href={component.href}
-                            >
-                              {component.description}
-                            </NavigationSubItem>
-                          ))}
-                        </ul>
-                      </NavigationMenuContent>
-                    </NavigationMenuItem>
-                  )
-                })
-              }
-            </NavigationMenuList>
-          </NavigationMenu>
-          {/* {nav.map((i, idx) => (
+                        <NavigationMenuContent>
+                          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            {Array(6).fill({
+                              title: "Coming Soon",
+                              href: "#",
+                              description:
+                                "Keep an eye out and come back later. Big things coming soon.",
+                            }).map((component) => (
+                              <NavigationSubItem
+                                key={component.title}
+                                title={component.title}
+                                href={component.href}
+                              >
+                                {component.description}
+                              </NavigationSubItem>
+                            ))}
+                          </ul>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+                    )
+                  })
+                }
+              </NavigationMenuList>
+            </NavigationMenu>
+            {/* {nav.map((i, idx) => (
             <li key={idx}>
               <Link className="flex items-center h-8 px-3 rounded-md hover:bg-slate-100" href={i.route}>{i.label}</Link>
             </li>
           ))} */}
-        </ul>
-        {/* <div className="w-full mx-2 transition-colors duration-300 border-t border-amethyst-500 group-hover:border-green-400"></div> */}
-        <div className="items-center hidden gap-2 md:flex shrink-0">
-          <button className="flex items-center px-3 py-2 text-sm border-2 border-black hover:bg-black hover:text-white">Log In</button>
-          <Link href={routes.subscribe} className="hidden md:flex">
-            <button className="flex items-center px-3 py-2 text-sm text-white border-2 border-transparent hover:border-black bg-amethyst-500">Join Shift Forward</button>
-          </Link>
-          <LanguageSwitcher />
+          </ul>
+          <div className="items-center hidden gap-2 lg:flex shrink-0">
+            <button className="flex items-center px-3 py-2 text-sm border-2 border-black hover:bg-black hover:text-white">Log In</button>
+            <Link href={routes.subscribe} className="hidden md:flex">
+              <button className="flex items-center px-3 py-2 text-sm text-white border-2 border-transparent hover:border-black bg-amethyst-500">Join Shift Forward</button>
+            </Link>
+            <LanguageSwitcher />
+          </div>
         </div>
-        <div className="flex md:hidden">
+        <div className="flex lg:hidden">
           <Sheet>
             <SheetTrigger className="flex items-center justify-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12" /><line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /></svg>
