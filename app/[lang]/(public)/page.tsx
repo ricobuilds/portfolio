6523985eb06ace871be51ef28c8112eb3ef91e93
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-07 16:37:24 +0100

    update: banner

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -308,13 +308,13 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
               </div>
             </section>
             <section id="cta" className="px-6 border-t-2 border-black bg-jade-500 selection:bg-tingual-300">
-              <div className="flex flex-col md:flex-row items-center justify-between self-stretch gap-4 lg:gap-12 mx-auto py-9 max-w-[1360px] w-full">
-                <div className="flex items-center ml-6 shrink-0">
+              <div className="flex flex-col md:flex-row items-center justify-center self-stretch gap-4 lg:gap-12 mx-auto py-9 max-w-[1360px] w-full">
+                <div className="flex items-center shrink-0">
                   <Image src={'/images/ricobuilds.png'} alt="Enric Trillo Logo" width={64} height={64} className="inline w-8 h-8 transition-all duration-300 rounded-full" />
                 </div>
-                  <h2 className={cn(clash.className, "font-bold text-2xl lg:text-6xl")}>
-                    Stack skills to thrive, in the age of AI
-                  </h2>
+                <h2 className={cn(clash.className, "font-bold text-2xl lg:text-6xl")}>
+                  Stack skills to thrive, in the age of AI
+                </h2>
                 <button className="flex items-center gap-2 px-4 py-2 border-2 border-black shrink-0 hover:bg-black hover:text-white">
                   <span>Start Learning</span>
                   <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
