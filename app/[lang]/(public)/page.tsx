a68e9a20dae94fc9884773df7a095f5cec45aec1
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-07 16:51:19 +0100

    update: wip

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -253,7 +253,7 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
               </div>
               <section id="marquee" className="relative -z-10">
                 <div className="h-16 border-black bg-amethyst-500 border-y-2"></div>
-                <div className="flex items-center h-16 text-white -translate-y-10 -skew-y-3 bg-black border-black border-y-4">
+                <div className="flex items-center h-16 text-white -translate-y-10 -skew-y-3 bg-black border-black border-y-2">
                   <Marquee
                   >
                     {tl['home']['hero'].marquee.map((i, idx) => (
