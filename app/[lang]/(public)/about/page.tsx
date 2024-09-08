b552a11cf88d97386b9b518bedd93e5d3ddb1fde
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-08 13:24:09 +0100

    update: link to mckinsy report

--- app/[lang]/(public)/about/page.tsx
+++ app/[lang]/(public)/about/page.tsx
@@ -114,7 +114,7 @@ export default async function About({ params }: { params: { lang: Locale } }) {
               </section>
               <section id="mission" className="space-y-6">
                 <h3 className={cn(clash.className, "font-bold text-2xl uppercase w-full lg:w-fit px-4 py-1 text-center lg:text-left mb-3 bg-amethyst-500 text-white")}>Mission</h3>
-                <p>McKinsey reported in 2020 that they estimate close 400 million people globally to be displaced by emerging technologies by 2030.</p>
+                <p>In 2017, McKinsey <Link target="_blank" className="font-bold underline text-amethyst-500" href="https://www.mckinsey.com/featured-insights/future-of-work/jobs-lost-jobs-gained-what-the-future-of-work-will-mean-for-jobs-skills-and-wages">reported</Link> they estimat something between 400-800 million people globally to be displaced by emerging technologies by 2030.</p>
                 <p>The landscape is shifting fast, and my mission is to help people and businesses navigate this transformation. Whether through learning resources or software products, my goal is to assist others to thrive in the age of AI.</p>
               </section>
               <section id="what-i-do" className="hidden space-y-6">
