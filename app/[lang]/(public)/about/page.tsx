cf57e528250ac18b930ad92a5919623b5dccfcd9
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-08 13:09:28 +0100

    update: h2 > h3 in about page, recognition

--- app/[lang]/(public)/about/page.tsx
+++ app/[lang]/(public)/about/page.tsx
@@ -161,7 +161,7 @@ export default async function About({ params }: { params: { lang: Locale } }) {
                 </div>
               </section>
               <section id="recognition">
-                <h2 className={cn(clash.className, "font-bold text-2xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Awards & Recognition</h2>
+                <h3 className={cn(clash.className, "font-bold text-2xl uppercase w-fit px-4 py-1 mb-3 bg-amethyst-500 text-white")}>Awards & Recognition</h3>
                 <ul className="mb-0 ml-8 list-disc">
                   <li>
                     <strong>The Startup</strong>: Got featured by The Startup publication on Medium for <Link className="underline text-amethyst-500" href="https://medium.com/swlh/an-image-classifier-with-keras-2f0e9b868a36">documenting my work on AI</Link> and my academic journey.
