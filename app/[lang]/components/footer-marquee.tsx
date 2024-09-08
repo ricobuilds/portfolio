f9aaa75dfa5c900c146d53b021941ccd1911ad66
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-08 14:26:58 +0100

    update: base content to foortermarqe

--- app/[lang]/components/footer-marquee.tsx
+++ app/[lang]/components/footer-marquee.tsx
@@ -1,5 +1,17 @@
-const FooterMarquee = () => {
+import { cn } from "@/lib/shared-utils"
+import Marquee from "./magicui/marquee"
+import { clash } from "@/constants/fonts"
+
+
+export const FooterMarquee = () => {
   return (
-    <div className="py-4 sm:py-9"></div>
+    <div className="py-4 sm:py-9">
+      <Marquee>
+        <div className="flex items-center">
+          <span className={cn(clash.className, "text-xl font-bold uppercase")}>Shift Forward in the age of AI</span>
+          <span className="ml-4 text-xl">✦</span>
+        </div>
+      </Marquee>
+    </div>
   )
 }
