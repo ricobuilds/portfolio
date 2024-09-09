7cadea567a2a1bd770297b87a02e644cfa907433
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-09 18:20:26 +0100

    update: shdw > shift forward

--- app/[lang]/(public)/about/page.tsx
+++ app/[lang]/(public)/about/page.tsx
@@ -355,7 +355,7 @@ export default async function About({ params }: { params: { lang: Locale } }) {
                 </ul>
               </section>
               <section id="cta" className="space-y-6">
-                <h3 className={cn(clash.className, "font-bold text-2xl uppercase w-full lg:w-fit px-4 py-1 text-center lg:text-left mb-3 bg-amethyst-500 text-white")}>Shift Forward</h3>
+                <h3 className={cn(clash.className, "font-bold shadow-[8px_8px_0_0_#000] text-2xl uppercase w-full lg:w-fit px-4 py-1 text-center lg:text-left mb-3 bg-amethyst-500 text-white")}>Shift Forward</h3>
                 <p>I created Shift Forward to share what I learn about emerging tech, what I'm building, and spark a new generation of Shifters who thrive in hard times.</p>
                 <p className="mb-6">Join <strong>Shift Forward</strong> for weekly actionable insights on disruptive tech like Web3 and Robotics, and updates of what we're building at Metasyde.</p>
                 <div className="flex items-center gap-2">
