a64b77a910ac39276e3d1c39613e3269d04f1b31
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-09 18:00:06 +0100

    update: faq neo brutalism ui

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -370,12 +370,12 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
                 {faqs.map((faq, idx) => {
                   return (
                     <Accordion key={idx} type="single" collapsible className="w-full max-w-3xl mx-auto">
-                      <AccordionItem value={`item-${idx}`}>
-                        <AccordionTrigger className="flex items-center gap-4 justify-normal">
-                          <span>{faq.icon}</span>
+                      <AccordionItem value={`item-${idx}`} className="mb-4 border-2 border-black ">
+                        <AccordionTrigger className="flex items-center gap-4 justify-normal hover:bg-slate-100">
+                          <span className="ml-4">{faq.icon}</span>
                           <h3 className="text-xl">{faq.question}</h3>
                         </AccordionTrigger>
-                        <AccordionContent className="text-gray-600 dark:text-gray-300">
+                        <AccordionContent className="text-gray-600 border-t-2 border-black dark:text-gray-300 bg-amethyst-100">
                           {faq.answer}
                         </AccordionContent>
                       </AccordionItem>
