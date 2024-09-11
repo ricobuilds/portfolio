5d6687fd2a70a004ccc72ea7250a438063ded6f9
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-11 11:25:01 +0100

    update: wrote Tutorials sect copy

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -277,8 +277,8 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
                     <h3 className={cn(clash.className, "font-bold text-2xl lg:text-5xl")}>
                       Tutorials
                     </h3>
-                    <p className="mb-10 text-xl font-medium lg:text-3xl">Master new skills with hands-on interactive guides.</p>
-                    <p></p>
+                    <p className="mb-10 text-xl font-medium lg:text-3xl">Master new skills with hands-on interactive playgrouds.</p>
+                    <p className="mb-12 text-lg lg:text-2xl">Level up from Zero to Expert as you practice with real-world examples, labs and more.</p>
                     <button className="flex items-center gap-2 px-4 py-2 text-white transition bg-black border-2 border-black shrink-0 hover:bg-amethyst-500">
                       <span>Start Learning Today</span>
                       <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
@@ -314,7 +314,7 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
                       Glossary
                     </h3>
                     <p className="mb-10 text-xl font-medium lg:text-3xl">Understand the terms that are shaping disruptive technology.</p>
-                    <p className="mb-12 text-2xl">Buzzwords broken down so good, it should be a crime to get lost in the tech jargon.</p>
+                    <p className="mb-12 text-lg lg:text-2xl">Buzzwords broken down so good, it should be a crime to get lost in the tech jargon.</p>
                     <button className="flex items-center gap-2 px-4 py-2 text-white transition bg-black border-2 border-black shrink-0 hover:bg-amethyst-500">
                       <span>Explore the Glossary</span>
                       <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
