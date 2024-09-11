bfb5813f2b9e5cc5b8123b52e46ea3c981061365
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-11 14:02:35 +0100

    update: wip

--- app/[lang]/components/footer.tsx
+++ app/[lang]/components/footer.tsx
@@ -151,7 +151,7 @@ const Footer = ({
                 <p className="font-medium text-gray-900">Legal</p>
                 <ul className="mt-6 space-y-4 text-sm">
                   <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75"> Terms </a>
+                    <a href="#" className="text-gray-700 transition hover:opacity-75"> Terms of Services </a>
                   </li>
                   <li>
                     <a href="#" className="text-gray-700 transition hover:opacity-75"> Privacy Policy </a>
