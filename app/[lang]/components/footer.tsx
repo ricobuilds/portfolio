53333e899d1727f397ee7c002cbc18ab9db26dde
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-11 13:49:10 +0100

    update: legal > footer nav section

--- app/[lang]/components/footer.tsx
+++ app/[lang]/components/footer.tsx
@@ -125,7 +125,7 @@ const Footer = ({
                 </ul>
               </div>
               <div>
-                <p className="font-medium text-gray-900">Helpful Links</p>
+                <p className="font-medium text-gray-900">Legal</p>
                 <ul className="mt-6 space-y-4 text-sm">
                   <li>
                     <a href="#" className="text-gray-700 transition hover:opacity-75"> Contact </a>
