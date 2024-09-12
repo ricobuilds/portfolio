19dacce4ac64197764f989c12e8133fcbdf8cd46
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-09-12 23:00:24 +0100

    update: dict rename

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -4,7 +4,7 @@ import type { Locale } from '@/constants/i18n.config'
 const dictionaries = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   es: () => import("./dictionaries/es.json").then((module) => module.default),
-  pt: () => import("./dictionaries/pt.json").then((module) => module.default),
+  'pt-BR': () => import("./dictionaries/pt.json").then((module) => module.default),
 }
 
 export const getDictionary = async (locale: Locale) =>
