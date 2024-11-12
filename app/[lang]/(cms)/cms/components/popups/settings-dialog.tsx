877d5d4e824499c40ef42a8579ef37fd0271c23f
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-11-12 18:24:57 +0000

    update: x

--- app/[lang]/(cms)/cms/components/popups/settings-dialog.tsx
+++ app/[lang]/(cms)/cms/components/popups/settings-dialog.tsx
@@ -7,6 +7,7 @@ import { useUIStore } from "@/stores/ui-store"
 import { useState } from "react"
 import { useParams, useRouter } from "next/navigation"
 import { handleDeleteBucket } from "../../actions"
+import { capitalise } from "@/lib/shared-utils"
 
 export function SettingsDialog() {
   const [bucket, setBucket] = useState("")
@@ -32,15 +33,16 @@ export function SettingsDialog() {
 
   return (
     <Dialog open={openSettings} onOpenChange={setOpenSettings}>
-      <DialogContent className="sm:max-w-[640px] p-0 outline-none">
-        <Card>
-          <div className="flex flex-row items-center justify-between p-3 py-4 border-b">
-            <CardTitle className="text-xs font-medium">Blog Settings</CardTitle>
-            <div>
-              <RiAddBoxFill className="w-4 h-4" />
-            </div>
+      <DialogContent className="sm:max-w-[640px] h-[444px] p-0 outline-none">
+        <div className="flex flex-row text-xs items-center justify-between h-14 p-4 border-b">
+          <p className="font-medium">{capitalise(collection as string)} Schema</p>
+          <div className="flex gap-2">
+            Add Custom Fields
+            <RiAddBoxFill className="w-4 h-4" />
           </div>
-          <div className="flex flex-row w-full border-b divide-x-[1px]">
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-1 flex-row w-full border-b divide-x-[1px]">
             <div className="max-w-[40%] w-full p-4">
               attributes
             </div>
@@ -48,15 +50,15 @@ export function SettingsDialog() {
               props
             </div>
           </div>
-          <div className="flex flex-col gap-3 p-4 py-6">
-            <p className="text-sm font-medium"> → Please type in the name of the bucket to confirm removal.</p>
-            <Input placeholder="Enter the bucket's name" onChange={(e) => setBucket(e.target.value)} />
-            {error && (
-              <span className="text-scarlet-500">The bucket name entered does not match the current active bucket!</span>
-            )}
-            <button onClick={() => deleteBucket(bucket)} className="px-3 py-2 text-white border rounded-md bg-scarlet-500">I understand the consequences, delete this bucket</button>
-          </div>
-        </Card>
+        </div>
+        <div className="flex flex-col gap-3 p-4 py-6">
+          <p className="text-sm font-medium"> → Please type in the name of the bucket to confirm removal.</p>
+          <Input placeholder="Enter the bucket's name" onChange={(e) => setBucket(e.target.value)} />
+          {error && (
+            <span className="text-scarlet-500">The bucket name entered does not match the current active bucket!</span>
+          )}
+          <button onClick={() => deleteBucket(bucket)} className="px-3 py-2 text-white border rounded-md bg-scarlet-500">I understand the consequences, delete this bucket</button>
+        </div>
       </DialogContent>
     </Dialog>
   )
