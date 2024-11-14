d8c0481e0652c51a01fa96d7a937e7f669309928
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-11-14 11:39:25 +0000

    update: x

--- app/[lang]/(cms)/cms/components/popups/editor-panel.tsx
+++ app/[lang]/(cms)/cms/components/popups/editor-panel.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { Schema } from "@/lib/sdk";
+import { Schema, SchemaField } from "@/lib/sdk";
 import { cn } from "@/lib/shared-utils";
 import { useUIStore } from "@/stores/ui-store";
 import { RiCloseCircleFill, RiMore2Fill } from "@remixicon/react";
@@ -50,16 +50,11 @@ export function EditorPanel({ schema }: { schema: Schema }) {
             </div>
           </header>
         </div>
-        {/* {schema.fields.map((field) => (
+        {schema.fields.map((field) => (
           <div key={field.name}>
-            <Label htmlFor={field.name}>{field.label}</Label>
-            <Input
-              id={field.name}
-              value={formData[field.name] || ''}
-              onChange={(e) => handleInputChange(field.name, e.target.value)}
-            />
+            {renderFieldContent('', field)}
           </div>
-        ))} */}
+        ))}
         <div className="">
           <Label htmlFor="title">Title</Label>
           <Input id="title" className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300" placeholder="What's New" />
@@ -72,7 +67,6 @@ export function EditorPanel({ schema }: { schema: Schema }) {
             enrictrillo.com/blog/whats-new
           </p>
         </div>
-
         <div className="space-y-2">
           <Label htmlFor="date">Date</Label>
           <Input
@@ -90,4 +84,71 @@ export function EditorPanel({ schema }: { schema: Schema }) {
       </SheetContent>
     </Sheet>
   )
+}
+
+function renderFieldContent(value: any, field?: SchemaField) {
+  if (!field) return "No active component in [renderFieldContent]"
+
+  switch (field.type) {
+    case 'id':
+      return (
+        <div className="">
+          <Label htmlFor={field.name}>{field.label}</Label>
+          <Input value={value || "1213"} disabled />
+        </div>
+      )
+    case 'text':
+      return field.name === 'title' ? (
+        <div className="">
+          <Label htmlFor={field.name}>{field.label}</Label>
+          <Input id={field.name} className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300" placeholder="What's New" />
+        </div>
+      ) : (
+        <div className="">
+          <Label htmlFor="title">Title</Label>
+          <Input id="title" className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300" placeholder="What's New" />
+        </div>
+      )
+    case 'slug':
+      return (
+        <div className="border-scarlet-500 border">
+          <Label htmlFor="slug">{field.label}</Label>
+          <Input id="slug" className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300" placeholder="whats-new" />
+          <p className="flex items-center gap-2 mt-2 text-xs text-obsidian-400">
+            <span><Globe className="w-4 h-4" /></span>
+            enrictrillo.com/blog/whats-new
+          </p>
+        </div>
+      )
+    case 'date':
+      return field.name === "created" || "updated" ? (
+        <div className="space-y-2">
+          <Label htmlFor="date">{field.label}</Label>
+          <Input
+            id={field.name}
+            name={field.name}
+            type="date"
+            className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300"
+            value={value}
+          />
+        </div>
+      ) : (
+        <div className="space-y-2">
+          <Label htmlFor="date">Date</Label>
+          <Input
+            id="date"
+            name="date"
+            type="date"
+            className="px-2.5 py-[1px] bg-slate-100 border border-obsidian-300"
+          />
+        </div>
+      )
+    // return value ? format(new Date(value), 'dd/MM/yyyy, HH:mm') : ''
+    case 'boolean':
+      return value ? 'Yes' : 'No'
+    case 'rich-text':
+      return value ? `${value.substring(0, 50)}...` : ''
+    default:
+      return String(value || '')
+  }
 }
